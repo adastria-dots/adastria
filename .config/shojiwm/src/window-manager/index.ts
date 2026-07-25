b4dd935244812a16f3c1455ca13711c7b62ac645
hq9afk <hieubuiduc1@gmail.com>
2026-07-25 20:38:28 +0700

    update

--- .config/shojiwm/src/window-manager/index.ts
+++
@@ -1,4 +0,0 @@
-export { WindowManager } from "./manager";
-export { Workspace } from "./workspace";
-export { playRectAnimation, stopRectAnimation, type RectAnimationOptions } from "./rect-animation";
-export * from "./state";
