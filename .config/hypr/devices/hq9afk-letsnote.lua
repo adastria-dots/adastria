71f3cb76d0bcdafb88ae6e6c74617257f2c5159c
hq9afk <hieubuiduc1@gmail.com>
2026-07-26 18:13:18 +0700

    update

--- .config/hypr/devices/hq9afk-letsnote.lua
+++ .config/hypr/devices/hq9afk-letsnote.lua
@@ -1,22 +1,13 @@
 local B = require("utils.bootstrap")
 local L = require("utils.layout")
 
-local monitors = {
-	{ "eDP-1", "preferred", "0x0" },
-}
+local monitors = {{ "eDP-1", "preferred", "0x0" }}
 
 B.setup_displays(monitors)
 
 L.register(2)
 
 hl.config({ input = { scroll_method = "no_scroll" } })
-
-hl.monitor({
-	output = "DP-2",
-	mode = "preferred",
-	position = "auto",
-	mirror = "eDP-1",
-})
 
 B.auto_start({ 
 	"fcitx5",
