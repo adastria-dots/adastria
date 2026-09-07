e58f167ada2871217842121db1095e2f40572833
hq9afk <hieubuiduc1@gmail.com>
2026-09-07 18:41:59 +0700

    update

--- .config/hypr/devices/hq9afk.lua
+++ .config/hypr/devices/hq9afk.lua
@@ -7,6 +7,6 @@ B.setup_displays({
 	{ "HEADLESS", "1920x1080", "1600x1440" },
 	{ "DP-1", "2560x688@60", "5120x0", nil, 3 },
 })
-B.auto_start({ "fcitx5", "kokusei", "sunshine" })
+B.auto_start({ "fcitx5", "kokusei start-penance", "sunshine" })
 L.register({ ["DP-3"] = 3, ["DP-2"] = 2 })
 
